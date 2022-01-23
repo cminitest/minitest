8ee671e85cb865e884b57f307c0ac3c567c943dd
Benjamin Anderson <andeb2804@gmail.com>
2022-01-23 00:37:25 -0600

    Add person constructor condition

--- test/mocks_test.c
+++ test/mocks_test.c
@@ -90,7 +90,11 @@ describe("MiniTest", minitest_mocks)
       when("a variety of parameter types are passed")
         mock(person_constructor) no_return
         it("asserts the mock was called")
-          __wrap_person_constructor("Fry", 3000);
+          #if LD_WRAP
+            person_constructor("Fry", 3000);
+          #else
+            __wrap_person_constructor("Fry", 3000);
+          #endif
           expect(mock_for(person_constructor)) to have been called_with("Fry", 3000)
           expect(mock_for(person_constructor)) to not have been called_with("Fry", "3000")
         end
